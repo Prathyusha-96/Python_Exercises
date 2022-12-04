ab05c934ac8d4c407a759801b6785206d9061769
Prathyusharam <prathyushaa1996@gmail.com>
2022-12-04 21:08:06 +0530

    update file handlings

--- Day3/file-handlings.py
+++ Day3/file-handlings.py
@@ -7,3 +7,12 @@ f.close()
 # f = open('myfile.txt', 'x')
 f = open('myfile.txt', 'a')
 f.write('heloo how are you')
+
+import os
+os.remove("demofile.txt")
+
+# import os
+# if os.path.exists("demofile.txt"):
+#    os.remove("demofile.txt")
+# else:
+#   print("The file does not exist")
